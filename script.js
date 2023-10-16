125da00de8399024cef357f4bdff3be2a21a571a
rahulbatra-19 <batrarahul409@gmail.com>
2023-10-16 19:19:54 +0530

    added new skills

--- script.js
+++ script.js
@@ -1,68 +1,59 @@
-
-var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+var navMenuAnchorTags = document.querySelectorAll(".nav-menu a");
 var interval;
 
-
 for (var i = 0; i < navMenuAnchorTags.length; i++) {
-    navMenuAnchorTags[i].addEventListener('click', function (event) {
-        event.preventDefault();
-        var targetSectionID = this.textContent.trim().toLowerCase();
-        console.log(this.textContent);
-        var targetSection = document.getElementById(targetSectionID);
-        console.log(targetSection);
-        //    interval = setInterval(scrollVertically, 20, targetSection);
+  navMenuAnchorTags[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    var targetSectionID = this.textContent.trim().toLowerCase();
+    console.log(this.textContent);
+    var targetSection = document.getElementById(targetSectionID);
+    console.log(targetSection);
+    //    interval = setInterval(scrollVertically, 20, targetSection);
 
-        interval = setInterval(function () {
-            scrollVertically(targetSection);
-        }, 20);
-    });
+    interval = setInterval(function () {
+      scrollVertically(targetSection);
+    }, 20);
+  });
 }
-
 
 function scrollVertically(targetSection) {
-    var targetSectionCoordinates = targetSection.getBoundingClientRect();
-    if (targetSectionCoordinates.top <= 0) {
-        clearInterval(interval);
-        return;
-    }
-    window.scrollBy(0, 50);
+  var targetSectionCoordinates = targetSection.getBoundingClientRect();
+  if (targetSectionCoordinates.top <= 0) {
+    clearInterval(interval);
+    return;
+  }
+  window.scrollBy(0, 50);
 }
 
-
-var progressBars = document.querySelectorAll('.skill-progress > div');
-var skillContainer = document.getElementById('skill-container');
-window.addEventListener('scroll', checkScroll);
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillContainer = document.getElementById("skill-container");
+window.addEventListener("scroll", checkScroll);
 var animationDone = false;
-function initailiseBars(){
-    for(let bar of progressBars)
-    {
-        bar.style.width= 0 +'%';
-    }
+function initailiseBars() {
+  for (let bar of progressBars) {
+    bar.style.width = 0 + "%";
+  }
 }
 initailiseBars();
 
-function fillBars(){
-    for (let bar of progressBars)
-    {
-        let targetWidth= bar.getAttribute('data-bar-width');
-        let currentWidth =0;
-        let interval = setInterval(function(){
-            if(currentWidth> targetWidth)
-            {
-                clearInterval(interval);
-                return;
-            }
-            currentWidth++;
-            bar.style.width = currentWidth + '%';
-        }, 1);
-
-    }
-
+function fillBars() {
+  for (let bar of progressBars) {
+    let targetWidth = bar.getAttribute("data-bar-width");
+    let currentWidth = 0;
+    let interval = setInterval(function () {
+      if (currentWidth > targetWidth) {
+        clearInterval(interval);
+        return;
+      }
+      currentWidth++;
+      bar.style.width = currentWidth + "%";
+    }, 1);
+  }
 }
-function checkScroll(){
-    var coordinates = skillContainer.getBoundingClientRect();
-    if(!animationDone && coordinates.top < window.innerHeight){
-        animationDone = true;
-        fillBars();
-    }
+function checkScroll() {
+  var coordinates = skillContainer.getBoundingClientRect();
+  if (!animationDone && coordinates.top < window.innerHeight) {
+    animationDone = true;
+    fillBars();
+  }
 }
